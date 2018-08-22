f11643f9c678557f62bd218c4dfeb232109e056e
Joshua Ling <joshling1919@gmail.com>
2018-08-22 12:00:43 -0700

    Set up hacky way of live reloading through nodemon

--- src/client/App.js
+++ src/client/App.js
@@ -15,7 +15,7 @@ const AppContainer = styled.div`
 const App = () => (
   <AppContainer className="app">
     <h1>
-Josh Ling's portfolio
+Josh Ling's portfolio hacky
     </h1>
   </AppContainer>
 );
