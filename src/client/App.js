421b6b63fc86a4de9f07f18e482ba6bfc50e1047
Joshua Ling <joshling1919@gmail.com>
2018-10-08 22:31:42 -0700

    Add default font family

--- src/client/App.js
+++ src/client/App.js
@@ -1,24 +1,26 @@
 import React from 'react';
-import styled, { injectGlobal } from 'styled-components';
+import { injectGlobal } from 'styled-components';
 import styledNormalize from 'styled-normalize';
 import { hot } from 'react-hot-loader';
 import Home from './components/homepage';
 
+// eslint-disable-next-line
 injectGlobal`
   ${styledNormalize}
   html, body, #root, #root>div {
     height: 100%;
+    box-sizing: border-box;
+    font-family: Nunito, sans-serif;
+  }
+  a {
+    text-decoration: none;
   }
 `;
 
-const AppContainer = styled.div`
-  box-sizing: border-box;
-`;
-
 const App = () => (
-  <AppContainer className="app">
+  <div className="app">
     <Home />
-  </AppContainer>
+  </div>
 );
 
 export default hot(module)(App);
--- src/client/components/nav/Nav.jsx
+++ src/client/components/nav/Nav.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
 
+// sections
+// 1. blog: all my blog posts
+// categories of blog posts:
+// a) hobbies- piano, food, sports
+// b) beliefs- philosophical, political, religious
+// c) tech?
+// d) education
+// so maybe in the beginning the layout won't cater to multiple cateogries
+// but if I actually make enough posts, then I'll do a redesign to more easily
+// see all the different categories
+// 2. about me:
+// subsections:
+// a) brief summary of me and my interests/passions
+// b) resume/professional accomplishments
+// c) personal projects
+
 const Nav = () => (
-  <div>
-this is nav
-  </div>
+  <nav>
+    <a href="#posts">
+Posts
+    </a>
+    <a href="#about">
+About Me
+    </a>
+  </nav>
 );
 
 export default Nav;
