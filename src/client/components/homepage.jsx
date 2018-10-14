b928a41a711b84571ea8cd4565d9afe582509265
Joshua Ling <joshling1919@gmail.com>
2018-10-14 09:55:01 -0700

    Add a styled body

--- src/client/components/homepage.jsx
+++ src/client/components/homepage.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import styled from 'styled-components';
 import Nav from './nav/Nav';
 import Posts from './posts/Posts';
 import About from './about/About';
 
+const Body = styled.div`
+  padding: 25px 20px;
+`;
+
 const Home = () => (
-  <Router>
-    <div>
-      <Nav />
+  <div>
+    <Nav />
+    <Body>
       <Route exact path="/" component={Posts} />
       <Route exact path="/about" component={About} />
-    </div>
-  </Router>
+    </Body>
+  </div>
 );
 
 export default Home;
